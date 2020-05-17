c9f16091f5cde8f0ce89bb23ca798cf7987d5cfd
Roman Droga <romandroga@gmail.com>
2020-05-17 16:56:49 +0300

    Updates

---
+++ precache-manifest.a2282b7e98f9e21e91230630e33b2441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd452c6a1a6ce10344f26ce482b1d845",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "8f54f319c79593eb60ec",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.828d14c0.chunk.css"
+  },
+  {
+    "revision": "0999ae2eeb2f7990ee7e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e0a51bcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.e0a51bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f54f319c79593eb60ec",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.6b8917b5.chunk.js"
+  },
+  {
+    "revision": "2c33b5407f6ddb0305a4",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.395ab540.js"
+  }
+]);
